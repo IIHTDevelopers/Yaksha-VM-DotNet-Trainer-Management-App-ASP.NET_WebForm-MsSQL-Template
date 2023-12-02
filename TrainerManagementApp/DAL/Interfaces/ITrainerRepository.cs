15e6d07a06a52928f814b4663cb6a3b1ddaa2139
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-02 10:34:04 +0530

    Add files via upload

---
+++ TrainerManagementApp/DAL/Interfaces/ITrainerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainerManagementApp.Model;
+
+namespace TrainerManagementApp.DAL.Interfaces
+{
+    public interface ITrainerRepository
+    {
+        string GetAll();
+        string Add();
+        string Update();
+        string Delete();
+
+        TrainerModel GetById(string id);
+
+    }
+}
