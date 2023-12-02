4dea2d8dd212c342c8814bc0b48a69411824e4d4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-02 13:23:59 +0530

    Add files via upload

---
+++ TrainerManagement.Tests/TestCases/BoundaryTests.cs
@@ -0,0 +1,331 @@
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainerManagementApp.DAL.Interfaces;
+using TrainerManagementApp.DAL.Services;
+using TrainerManagementApp.Model;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TrainerManagementApp.Tests.TestCases
+{
+    public class BoundaryTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ITrainerService _trainerService;
+        private readonly TrainerModel _trainer;
+        public readonly Mock<ITrainerRepository> trainerservice = new Mock<ITrainerRepository>();
+
+        private static string type = "Boundary";
+
+        public BoundaryTests(ITestOutputHelper output)
+        {
+            _trainerService = new TrainerService(trainerservice.Object);
+            _output = output;
+            _trainer = new TrainerModel
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = new DateTime(1985, 5, 10)
+            };
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Add_Trainer_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Add()).Returns("");
+                var result = _trainerService.Add();
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Delete_Trainer_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Delete()).Returns("");
+                var result = _trainerService.Delete();
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_UpdateTrainer_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Update()).Returns("");
+                var result = _trainerService.Update();
+
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_GetByIdTrainer_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.GetById("1")).Returns(_trainer);
+                var result = _trainerService.GetById("1");
+
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_GetByAll_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.GetAll()).Returns("");
+                var result = _trainerService.GetAll();
+
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_AddTrainer_ReturnsString()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Add()).Returns("");
+                var result = _trainerService.Add();
+
+                //Assertion
+                if (result is string)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_DeleteTrainer_ReturnsString()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Delete()).Returns("");
+                var result = _trainerService.Delete();
+
+                //Assertion
+                if (result is string)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+    }
+}
---
+++ TrainerManagement.Tests/TestCases/ExceptionalTests.cs
@@ -0,0 +1,112 @@
+﻿
+
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrainerManagementApp.DAL.Interfaces;
+using TrainerManagementApp.DAL.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TrainerManagementApp.Tests.TestCases
+{
+    public class ExceptionalTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ITrainerService _trainerService;
+        public readonly Mock<ITrainerRepository> trainerservice = new Mock<ITrainerRepository>();
+
+        private static string type = "Exception";
+
+        public ExceptionalTests(ITestOutputHelper output)
+        {
+            _trainerService = new TrainerService(trainerservice.Object);
+            _output = output;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_GetAll_Trainers_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.GetAll()).Returns("");
+                var result = _trainerService.GetAll();
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Update_Trainers_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                trainerservice.Setup(repos => repos.Update()).Returns("");
+                var result = _trainerService.Update();
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+
+    }
+}
