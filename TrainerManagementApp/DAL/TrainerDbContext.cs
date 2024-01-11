efe18f7d2491231ad2aba0ca3d159bdbee72373e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:24:05 +0530

    Add files via upload

---
+++ TrainerManagementApp/DAL/TrainerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TrainerManagementApp.Model;
+
+namespace TrainerManagementApp.DAL
+{
+    public class TrainerDbContext : DbContext
+    {
+        public DbSet<Model.TrainerModel> TrainerModels { get; set; }
+
+        public TrainerDbContext() : base("ConnStr")
+        {
+        }
+    }
+}
