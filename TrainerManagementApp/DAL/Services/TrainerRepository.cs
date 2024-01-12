c014a5ebf221385b4e6b4882de0753b2f2a80572
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-12 14:20:47 +0530

    Add files via upload

---
+++ TrainerManagementApp/DAL/Services/TrainerRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TrainerManagementApp.DAL.Interfaces;
+
+namespace TrainerManagementApp.DAL.Services
+{
+    public class TrainerRepository : Interfaces.ITrainerRepository
+    {
+        private TrainerDbContext _context;
+
+        public TrainerRepository(TrainerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.TrainerModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrainerManagementApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainerManagementApp.Global" Language="C#" %>
